ae474897ca5a24a59550621d6d6da32b5e8057db
christian <christianjones94@gmail.com>
2023-01-10 17:07:55 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import postRoutes from "./Routes/posts.js";
+
+const app = express();
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://christianJones:christianJones123@cluster0.ihxmrr2.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .set("strictQuery", false)
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+  })
+  .catch((error) => console.log(error.message));
